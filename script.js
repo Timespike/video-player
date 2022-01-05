cd343a3c4fb97fc229fb2fb8fdc4dd586c8915c1
Timespike <50344633+Timespike@users.noreply.github.com>
2022-01-04 19:30:12 -0600

    fixed a bug with the currentTime constant

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const playBtn = document.getElementById('play-btn');
 const volumeIcon = document.getElementById('volume-icon');
 const volumeRange = document.querySelector('.volume-range');
 const volumeBar = document.querySelector('.volume-bar');
-const currentTime = document.querySelector('time-elapsed');
+const currentTime = document.querySelector('.time-elapsed');
 const duration = document.querySelector('.time-duration');
 const fullscreenBtn = document.querySelector('.fullscreen');
 
@@ -33,9 +33,19 @@ video.addEventListener('ended', showPlayIcon);
 
 // Progress Bar ---------------------------------- //
 
+// Calculate display time format
+function displayTime(time) {
+    const minutes = Math.floor(time / 60);
+    let seconds = Math.floor(time % 60);
+    seconds = seconds > 9 ? seconds : `0${seconds}`; 
+    return `${minutes}:${seconds}`;
+}
+
 // Update progress bar as video plays
 function updateProgress() {
     progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
+    currentTime.textContent = `${displayTime(video.currentTime)} /`;
+    duration.textContent = `${displayTime(video.duration)}`;
 }
 
 
