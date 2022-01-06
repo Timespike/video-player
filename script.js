20afa871d077ab631becb8e7c6bdbc3b5c471419
Timespike <50344633+Timespike@users.noreply.github.com>
2022-01-05 18:17:03 -0600

    mute button

--- script.js
+++ script.js
@@ -56,6 +56,8 @@ function setProgress(e) {
 
 // Volume Controls --------------------------- //
 
+let lastVolume = 1;
+
 // Volume Bar
 function changeVolume(e) {
     let volume = e.offsetX / volumeRange.offsetWidth;
@@ -79,7 +81,24 @@ function changeVolume(e) {
     } else if (volume === 0) {
         volumeIcon.classList.add('fas', 'fa-volume-off');
     }
-    
+    lastVolume = volume;
+}
+
+// Mute/Unmute
+function toggleMute() {
+    if (video.volume) {
+        volumeIcon.className = '';
+        lastVolume = video.volume;
+        video.volume = 0;
+        volumeBar.style.width = 0;
+        volumeIcon.classList.add('fas', 'fa-volume-mute');
+        volume.setAttribute('title', 'Unmute');
+    } else {
+        video.volume = lastVolume;
+        volumeBar.style.width = `${lastVolume * 100}%`;
+        volumeIcon.classList.add('fas', 'fa-volume-up');
+        volume.setAttribute('title', 'Mute');
+    }
 }
 
 
@@ -96,3 +115,4 @@ video.addEventListener('timeupdate', updateProgress);
 video.addEventListener('canplay', updateProgress);
 progressRange.addEventListener('click', setProgress);
 volumeRange.addEventListener('click', changeVolume);
+volumeIcon.addEventListener('click', toggleMute);
